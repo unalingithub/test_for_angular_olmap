c5feccc1565cd5081d1398a4bc852008661efdd0
RueiyunLin <610735008@gms.ndhu.edu.tw>
2022-11-18 16:08:01 +0800

    地圖初始畫面

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+
+}
+// import { Component, OnInit, ChangeDetectionStrategy, Input, ElementRef } from '@angular/core';
+// import Map from 'ol/Map';
+
+// @Component({
+//   selector: 'app-map',
+//   template: '',
+//   styles: [':host { width: 100%; height: 100%; display: block; }',
+//   ],
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+// })
+// export class MapComponent implements OnInit {
+//   @Input() map: Map;
+//   constructor(private elementRef: ElementRef) {
+//   }
+//   ngOnInit() {
+//     this.map.setTarget(this.elementRef.nativeElement);
+//   }
+// }
